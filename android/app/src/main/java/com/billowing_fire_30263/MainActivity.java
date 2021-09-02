09e4fd118aa1052f68eef547c50da0c1d833e2b6
Crowdbotics <team@crowdbotics.com>
2021-09-02 03:02:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/billowing_fire_30263/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billowing_fire_30263;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "billowing_fire_30263";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_fire_30263Identifier",
+  url: "https://billowing_fire_30263Identifier.botics.co",
+  api: "https://billowing_fire_30263Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
